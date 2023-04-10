1becd2bfa0528487b1fbe93e1446734cc0f1c928
sudhanshu <sudhanshu3430@gmail.com>
2023-04-11 01:42:51 +0530

    chatting enabled

---
+++ app/src/main/java/com/example/advchatapp/activities/firebase/MessagingService.java
@@ -0,0 +1,23 @@
+package com.example.advchatapp.activities.firebase;
+
+
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+    }
+}
---
+++ app/src/main/java/com/example/advchatapp/activities/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.example.advchatapp.activities.listeners;
+
+import com.example.advchatapp.activities.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
