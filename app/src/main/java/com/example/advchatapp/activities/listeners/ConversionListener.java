0f75e60d1cfaf040a3f1995bfa0be91163ea9c9d
sudhanshu <sudhanshu3430@gmail.com>
2023-04-14 01:31:50 +0530

    final commit

---
+++ app/src/main/java/com/example/advchatapp/activities/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.example.advchatapp.activities.listeners;
+
+import com.example.advchatapp.activities.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
